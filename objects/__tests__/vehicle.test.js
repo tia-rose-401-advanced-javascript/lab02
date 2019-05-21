a69e9d5d1bce9224c4fcefe20a3a85a07e80a8e1
Tia Rose <shortyshort789@aol.com>
2019-05-21 14:54:54 -0700

    have factory refactored. have factory test passing

--- objects/__tests__/vehicle.test.js
+++ objects/__tests__/vehicle.test.js
@@ -160,6 +160,87 @@ describe('Vehicles', () => {
 
 });
 
+// Factory Tests
+
+'use strict';
+
+const VehicleFactory = require('../vehicle-factory.js');
+
+let typeFac = ['Factory'];
+
+describe('Vehicles', () => {
+
+  describe('Car', () => {
+
+    function getCar(type) {
+      switch(type) {
+      case 'Factory':
+        return new VehicleFactory.Car('foo');
+      default:
+        return {};
+      }
+    }
+
+    typeFac.forEach( type => {
+
+      let car = getCar(type);
+
+      it(`${type} (Car) has 4 wheels`, () => {
+        expect(car.wheels).toEqual(4);
+      });
+
+      it(`${type} (Car) can drive`, () => {
+        expect(car.drive()).toBeTruthy();
+      });
+
+      it(`${type} (Car) can stop`, () => {
+        expect(car.stop()).toBeTruthy();
+      });
+
+      it(`${type} (Car) cannot do a wheelie`, () => {
+        expect(car.wheelie).toBeUndefined();
+      });
+    });
+
+  });
+
+  describe(`Motorcycle`, () => {
+
+    function getMotorcycle(type) {
+      switch(type) {
+      case 'Factory':
+        return new VehicleFactory.Motorcycle('foo');
+      default:
+        return {};
+      }
+    }
+
+    typeFac.forEach( type => {
+
+      let motorcycle = getMotorcycle(type);
+
+      it(`${type} (Motorcycle) has 2 wheels`, () => {
+        expect(motorcycle.wheels).toEqual(2);
+      });
+
+      it(`${type} (Motorcycle) can drive`, () => {
+        expect(motorcycle.drive()).toBeTruthy();
+      });
+
+      it(`${type} (Motorcycle) can stop`, () => {
+        expect(motorcycle.stop()).toBeTruthy();
+      });
+
+      it(`${type} (Motorcycle) cannot do a wheelie`, () => {
+        expect(motorcycle.wheelie()).toBeTruthy();
+      });
+
+    });
+
+  });
+
+});
+
 
 
 
