c0ef1113794511ea1fb4ab3ab60bdd28034c113a
Tia Rose <shortyshort789@aol.com>
2019-05-21 20:12:06 -0700

    have list class passing test. moving on to constructor

--- list/list.js
+++ list/list.js
@@ -27,6 +27,39 @@ class List {
     this.length--;
     return returnValue;
   }
+  /**
+   * // Removes the first item from a list and return the remaining array
+   * 
+   */
+  shift(){
+    let returnValue = this.data[this.length];
+    delete this.data[0];
+    this.length--;
+    return returnValue;
+  }
+  /**
+   * // Adds elements to the begining of the array and returns the length
+   * 
+   */
+
+  unshift(item){
+    let returnValue = this.data[this.length];
+    for(let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return returnValue;
+  }
+  /**
+   * // Returns each element in the array
+   * 
+   */
+  forEach(){
+    for(let i = 0; i < this.length; i++){
+      this.data[i];
+    }
+  }
 
 }
 
